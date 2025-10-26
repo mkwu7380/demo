f321b40de67c0e0170b12d5da7d4ec1fd17c7cfc
Michael Wu <wuchunkin@hotmail.com>
2025-10-26 19:17:25 +0800

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,18 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
+}
+
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        the<KotlinJvmProjectExtension>().jvmToolchain(17)
+    }
+    plugins.withId("org.jetbrains.kotlin.android") {
+        the<KotlinAndroidProjectExtension>().jvmToolchain(17)
+    }
 }
